15836440a6b5988954fe327b44135da2d231473d
MarcosMel0 <marcos.melo@linx.com.br>
2025-04-29 00:54:30 -0300

    Ajuste em exposição de porta e ip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',   // <- força aceitar conexões de fora (não só localhost)
     port: 3000,
-    host: true,    // <- adiciona essa linha
-    open: false,   // <- evita tentar abrir navegador dentro do container
+    strictPort: true,   // <- garante erro se a porta 3000 já estiver em uso
+    open: false,        // <- não tenta abrir navegador
+    cors: true          // <- habilita CORS, caso seu front acesse APIs externas
   },
   build: {
     outDir: 'dist',
