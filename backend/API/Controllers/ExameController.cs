e045a7ac4224c4c56f13d5e44abc28b46956d20e
MarcosMel0 <marcos.melo@linx.com.br>
2025-04-29 00:00:12 -0300

    Rodando Backend + Front + banco + Docker

--- backend/API/Controllers/ExameController.cs
+++ backend/API/Controllers/ExameController.cs
@@ -1,7 +1,9 @@
 ﻿using backend.Domain.Entities;
+using backend.Infrastructure.Data;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using static backend.Application.Command.ExameCommand;
 
 namespace backend.API.Controllers
@@ -11,9 +13,12 @@ namespace backend.API.Controllers
     public class ExameController : ControllerBase
     {
         private readonly IMediator _mediator;
-        public ExameController(IMediator mediator)
+        private readonly ApplicationDbContext _context;
+
+        public ExameController(IMediator mediator, ApplicationDbContext context)
         {
             _mediator = mediator;
+            _context = context;  // Inicializa o _context
         }
 
         [HttpPost]
@@ -27,5 +32,20 @@ namespace backend.API.Controllers
             var id = await _mediator.Send(exame);
             return Ok(new { Id = id });
         }
+
+        [HttpGet("test-db")]
+        public async Task<IActionResult> TestDbConnection()
+        {
+            try
+            {
+                var count = await _context.Exames.CountAsync();
+                return Ok($"Conexão bem-sucedida! Exames no banco: {count}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro na conexão com o banco de dados: {ex.Message}");
+            }
+        }
     }
+
 }
